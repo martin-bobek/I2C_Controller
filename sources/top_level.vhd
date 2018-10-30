3b7fff5b2503b6bd0427bdd55f9a4208f50f02e6
martin-bobek <martinbobek8@gmail.com>
2018-10-29 20:30:50 -0600

    Button inputs get mapped to LED outputs.

--- sources/top_level.vhd
+++ sources/top_level.vhd
@@ -1,15 +1,17 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity top_level is 
+entity top_level is
   port (
-    a: in  std_logic;
-    b: in  std_logic;
-    c: out std_logic
+    button_1: in  std_logic;
+    button_2: in  std_logic;
+    led_1:    out std_logic;
+    led_2:    out std_logic
   );
 end;
 
 architecture rtl of top_level is
 begin
-  c <= a and b;
+  led_1 <= button_1;
+  led_2 <= button_2;
 end;
--- tests/tb_top_level.vhd
+++ tests/tb_top_level.vhd
@@ -6,32 +6,32 @@ entity top_level_tb is end;
 architecture rtl of top_level_tb is 
   constant period: time := 10ns;
   
-  signal a: std_logic;
-  signal b: std_logic;
-  signal c: std_logic;
+  signal button_1, button_2: std_logic;
+  signal led_1, led_2: std_logic;
 begin
   uut: entity work.top_level
     port map (
-      a => a,
-      b => b,
-      c => c
+      button_1 => button_1,
+      button_2 => button_2,
+      led_1    => led_1,
+      led_2    => led_2
     );
 
   process begin
-    a <= '0';
-    b <= '0';
+    button_1 <= '0';
+    button_2 <= '0';
     wait for period;
     
-    a <= '0';
-    b <= '1';
+    button_1 <= '0';
+    button_2 <= '1';
     wait for period;
     
-    a <= '1';
-    b <= '1';
+    button_1 <= '1';
+    button_2 <= '1';
     wait for period;
     
-    a <= '1';
-    b <= '0';
+    button_1 <= '1';
+    button_2 <= '0';
     wait for period;
   end process;
 end;
